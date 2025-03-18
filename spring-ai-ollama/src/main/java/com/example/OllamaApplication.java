fe725ecda898faa986545db18e1e912e05baf0d6
Eric <huageng.yang@cqbdri.pku.edu.cn>
2025-03-18 15:43:42 +0800

    fix : comment

--- spring-ai-ollama/src/main/java/com/example/OllamaApplication.java
+++ spring-ai-ollama/src/main/java/com/example/OllamaApplication.java
@@ -14,6 +14,12 @@ public class OllamaApplication {
         SpringApplication.run(OllamaApplication.class,  args);
     }
 
+    /**
+     * 可以设置系统提示词 defaultSystem
+     * 可以设置默认上下文长度 maxTokens
+     * @param builder
+     * @return
+     */
     @Bean
     ChatClient chatClient(ChatClient.Builder builder) {
         return builder.defaultSystem("你是一个AI助手，可以回答任何问题。")
